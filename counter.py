9e440f74d7a0fc2b441acf20d129a0b7d95eb3e5
raychore <raychore@gmail.com>
2018-09-09 22:24:43 +0800

    delete counter.py

--- counter.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-'''Counter NO. from 1 to 300'''
-for i in range(1,301):
-    aStr = str(i)
-    if i % 10 == 0:
-        print(aStr)
-    else:
-        print(aStr, end = "\t")
